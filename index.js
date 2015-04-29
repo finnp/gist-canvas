56bb0a31ece40c24399109eba3c448edb1c19675
Finn <derfinn@gmail.com>
2015-04-30 00:08:20 +0200

    nicer ui

--- index.js
+++ index.js
@@ -1,22 +1,46 @@
+var on = require('add-event-listener')
 var xhr = require('xhr')
 var hashchange = require('hashchange')
+var edit = require('edit')
 
 var canvas = document.querySelector('canvas')
+var $url = document.querySelector('#url')
+var $file = document.querySelector('#file')
+var $source = document.querySelector('#source')
+
+//<!--<script src="https://gist.github.com/finnp/a275a003473c6d95f6d8.js"></script>-->
 
 hashchange.update(function (hash) {
+  console.log('update')
+  if(!hash) return
+  $file.style.display = 'none'
+  $source.style.display = 'block'
   canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  
-  hash = hash || 'finnp/a275a003473c6d95f6d8'
+  canvas.height = window.innerHeight / 2
+
   var uri = 'https://gist.githubusercontent.com/' + hash
   var ctx = canvas.getContext('2d')
   var context = ctx
+  
+  var cm = edit({
+    autofocus: false
+  })
 
   xhr({
     uri: uri + '/raw'
   }, function (err, resp, body) {
     if(err) console.error(err)
+    cm.setValue(body)
     eval(body)
   })
 })
 hashchange.update()
+
+on($url, 'keydown', function (e) {
+  var gist = $url.value
+  // https://gist.github.com/finnp/64b628be54b9bbab3c24
+  var parts = gist.split('/')
+  var gistId = parts.pop()
+  var gistName = parts.pop()
+  if(e.keyCode === 13) window.location.hash = gistName + '/' + gistId
+})
